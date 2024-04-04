2fd9c312d3ff72b8aa6d75d6056dc4fd975b802c
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2024-04-04 05:42:56 +0530

    add isDuplicate Url test

--- test/isDuplicate.test.js
+++ test/isDuplicate.test.js
@@ -1,18 +1,14 @@
-const isDuplicate = require('../src/controllers/checkDuplicate')
+const isDuplicate = require('../src/controllers/checkDuplicate.js');
 
 describe('isDuplicate', () => {
     test('returns true for valid urls', async () => {
-        expect(await isDuplicate('https://www.google.com')).toBe(true);
-        expect(await isDuplicate('https://www.google.com')).toBe(
-            true
-        );
+        expect(await isDuplicate('https://www.facebook.com')).toBe(true);
     });
 
     test('returns false for invalid urls', async () => {
         expect(await isDuplicate('')).toBe(false);
         expect(await isDuplicate('www.example.com')).toBe(false);
         expect(await isDuplicate('example.com')).toBe(false);
-        // expect(await isDuplicate('http://example.com')).toBe(false);
         expect(await isDuplicate('https://exml.com')).toBe(false);
     });
 });
