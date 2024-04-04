16effc577713c179a19331208c7cbce2b381c7fc
Ayush Kumar <98096047+Ayush211107@users.noreply.github.com>
2024-04-04 06:48:58 +0530

    add url mongodb model

--- src/models/url.model.js
+++ src/models/url.model.js
@@ -1,4 +1,6 @@
-import mongoose from 'mongoose';
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
 // Define schema for URL model
 const urlSchema = new mongoose.Schema({
@@ -9,12 +11,21 @@ const urlSchema = new mongoose.Schema({
     },
     shortURL: {
         type: String,
-        required: true,
         unique: true
     },
 }, { timestamps: true });
 
+// Create a virtual property for the short URL
+urlSchema.virtual('fullShortURL').get(function () {
+    return process.env.BASE_URL + "/" + this._id;
+});
 
-const URL = mongoose.model('URL', urlSchema);
+// Define pre-save hook to set the short URL before saving
+urlSchema.pre('save', async function (next) {
+    if (!this.shortURL) {
+        this.shortURL = this.fullShortURL;
+    }
+    next();
+});
 
-module.exports = URL;
+module.exports = URL = mongoose.model('URL', urlSchema);
